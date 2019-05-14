8809d73cd44804228f1f356016c26f2e3d69fe76
barbell <barbell@users.noreply.github.com>
2019-05-14 09:59:30 +0200

    Add files via upload

---
+++ List_AD_On-Prem_users_with_all_properties.ps1
@@ -0,0 +1,3 @@
+﻿Import-Module ActiveDirectory
+
+Get-ADUser -Filter * -Properties * | select City,Company,Country,Department,Description,DisplayName,Division,EmailAddress,Enabled,Fax,GivenName,mobile,MobilePhone,Name,Office,OfficePhone,Organization,POBox,PostalCode,SamAccountName,sn,State,StreetAddress,Surname,Title,UserPrincipalName | Export-Csv -Append -Path C:\temp\all-ad-user-info_2019-04-23.csv
