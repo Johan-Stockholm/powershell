73dabce3295ed1176f1473f564bf5c47de77f076
Johan <barbell@users.noreply.github.com>
2019-05-14 11:42:32 +0200

    Add files via upload

---
+++ Copy_Users_from_one_AD_Security_Group_to_another_one.ps1
@@ -0,0 +1,6 @@
+﻿$source_group = Get-ADGroupMember -Filter {name -eq "Source Group"}
+$destination_group = Get-ADGroupMember -Filter {name -eq "Destination Group"}
+
+foreach ($user in $source_group) { 
+    Add-ADGroupMember -Identity $destination_group -Members $user.distinguishedname 
+}
